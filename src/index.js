f84899135774e57ca063cffc839ff7ee936fabe6
ambarnett <andy.barnett09@gmail.com>
2021-07-08 14:58:42 -0500

    inital commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { KandyKorner } from './components/KandyKorner'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <KandyKorner />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
